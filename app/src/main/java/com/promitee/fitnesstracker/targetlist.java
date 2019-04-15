8ec7b6d4309b2b38a07aef02ca46bc698e7db4b1
promitee <fatema.promitee@gmail.com>
2019-04-15 22:34:20 +0600

    initial Upload

---
+++ app/src/main/java/com/promitee/fitnesstracker/targetlist.java
@@ -0,0 +1,51 @@
+package com.promitee.fitnesstracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class targetlist extends AppCompatActivity {
+    private int client_id ;
+    private CheckBox wl;
+    private CheckBox rf;
+    private sqliteHelper database ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.targetlist);
+
+        database = new sqliteHelper(this) ;
+        wl = findViewById(R.id.wl) ;
+        rf = findViewById(R.id.rf) ;
+
+        Intent intent = getIntent() ;
+        client_id = intent.getExtras().getInt("c_id") ;
+        Log.d("In hweight",String.valueOf(client_id)) ;
+    }
+
+
+    public void finishReg(View view) {
+        int a = wl.isChecked()?1:0 ; //weight loss
+        int b = rf.isChecked()?1:0 ; //regular
+
+        if(a==1) {
+            database.insertToClientOptions(client_id, a, b, 5000);
+        }
+        if(b==1 & a!=1) {
+            database.insertToClientOptions(client_id, a, b, 2500);
+        }
+
+        Intent intent = new Intent(targetlist.this,dashboard.class) ;
+        intent.putExtra("client_id",client_id) ;
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed(){
+    }
+
+}
